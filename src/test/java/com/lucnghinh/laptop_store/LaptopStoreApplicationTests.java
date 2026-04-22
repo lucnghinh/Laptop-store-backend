868ce8ec4b9da326c6f52b742fc14fdbe5215ac9
Nguyen Ngoc Luc <lucnghinh1@gmail.com>
2026-04-22 22:36:36 +0700

    khởi tạo

---
+++ src/test/java/com/lucnghinh/laptop_store/LaptopStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucnghinh.laptop_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
